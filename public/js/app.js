6536fb8fe0f737ae0c1c9e0850c54dba1afc7aed
Jens Titterness <jenstitterness@gmail.com>
2015-03-22 01:04:32 -0700

    increase interval, added loop and another source.

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,7 @@ var videoList = [
   'http://tcdn.ustream.tv/video/45134284?preset_id=2&e=1427020537&h=84f1eaa8bca122ea14f1efb92b6affc8',
   'http://tcdn.ustream.tv/video/58437319?preset_id=2&e=1427020500&h=8dce9f98e443140517f6920b8f48d06d',
   'http://tcdn.ustream.tv/video/57152633?preset_id=2&e=1427020377&h=24ae7c205d19a212a35d3e8624d92957',
-  'http://sjc-uhls-proxy02.ustream.tv/watch/playlist.m3u8?cid=10371840&appType=11&appVersion=2&locks=97d170e1550eee4afc0af065b78cda302a97674c&geo=US&geocity=Seattle&userId=555456111&connectionId=sjc-flot-live04_555456111&ts=1427006808&ip=98.232.13.116&cdn=uhs_akamai&sgn=6b761bdf8f58086d1bcaaae03efd5a111643cccc',
+  'http://upmv09.ustream.tv/0/1/40/40771/40771275/1_12569659_40771275.mp4?e=1427616057&h=8445d996ab2906bd754d033075404456&tracking=b2094a_492_1_1_0&peer=COMCAST-PEER.COMCAST2-PEER',
   'http://api.new.livestream.com/accounts/4175709/events/3856342/broadcasts/81152060.m3u8?dw=100&hdnea=st=1427010068~exp=1427010968~acl=/i/4175709_3856342_fd42e63a_1@137093/*~hmac=a3688759e8b3d5432eba47485973c952be236ccdad544f0bbad37240a687b237'
 ];
 
@@ -20,7 +20,7 @@ var setVideo = function() {
   console.log("random:", random);
   console.log('loading:', videoList[random]);
 
-  $('body').empty().append('<video autoplay ></video>');
+  $('body').empty().append('<video autoplay loop></video>');
 
   if (regex.test(videoList[random])) {
     $("video").prop('type', "application/vnd.apple.mpegurl");
@@ -30,8 +30,11 @@ var setVideo = function() {
 };
 
 $(document).ready(function() {
+  var interval = 86400000; // 1 day
+
   setVideo();
-  var timer = setInterval(setVideo, 30000);
+
+  var timer = setInterval(setVideo, interval);
 
 
 });
