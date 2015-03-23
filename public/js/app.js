4abfad636480c5c1e31b2a55ed89def6478e4ac9
Jens Titterness <jenstitterness@gmail.com>
2015-03-22 22:49:59 -0700

    update sources

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,8 @@
 
 var videoList = [
-  'http://tcdn.ustream.tv/video/57701782?preset_id=2&e=1427020273&h=35939f909c2478072ba2d0b652cc951a',
-  'http://tcdn.ustream.tv/video/45134284?preset_id=2&e=1427020537&h=84f1eaa8bca122ea14f1efb92b6affc8',
-  'http://tcdn.ustream.tv/video/58437319?preset_id=2&e=1427020500&h=8dce9f98e443140517f6920b8f48d06d',
-  'http://tcdn.ustream.tv/video/57152633?preset_id=2&e=1427020377&h=24ae7c205d19a212a35d3e8624d92957',
-  'http://upmv09.ustream.tv/0/1/40/40771/40771275/1_12569659_40771275.mp4?e=1427616057&h=8445d996ab2906bd754d033075404456&tracking=b2094a_492_1_1_0&peer=COMCAST-PEER.COMCAST2-PEER'
+  'http://sjc-uhls-proxy03.ustream.tv/watch/playlist.m3u8?cid=13027369&appType=11&appVersion=2&locks=97d170e1550eee4afc0af065b78cda302a97674c&geo=US&geocity=Seattle&userId=807984045&connectionId=sjc-flot-live02_807984045&ts=1427088838&ip=98.232.13.116&cdn=uhs_akamai&sgn=99f752f52c6a75e36e65558225552a0799739536',
+  'http://sjc-uhls-proxy04.ustream.tv/watch/playlist.m3u8?cid=13607192&appType=11&appVersion=2&locks=97d170e1550eee4afc0af065b78cda302a97674c&geo=US&geocity=Seattle&userId=1206326944&connectionId=sjc-flot-live05_1206326944&ts=1427089211&ip=98.232.13.116&cdn=uhs_akamai&sgn=fccc0a32ee01b6cebc4253a594757f2e7fd39e83',
+  'http://sjc-uhls-proxy109.ustream.tv/watch/playlist.m3u8?cid=13027422&appType=11&appVersion=2&locks=97d170e1550eee4afc0af065b78cda302a97674c&geo=US&geocity=Seattle&userId=1888548696&connectionId=sjc-flot-live06_1888548696&ts=1427089442&ip=98.232.13.116&cdn=uhs_akamai&sgn=3b633d35dbe3a5ec6047c92db506496c15235d85'
 ];
 
 var getRandom = function(min, max) {
@@ -19,7 +17,7 @@ var setVideo = function() {
   console.log("random:", random);
   console.log('loading:', videoList[random]);
 
-  $('body').empty().append('<video autoplay loop></video>');
+  $('body').empty().append('<video autoplay loop type="application/vnd.apple.mpegurl"></video>');
 
   $('video').prop('src', videoList[random]);
 };
